ab8eb4269f04432d259ea1ea1d15539fe8d65231
Valerius Mattfeld <git@valerius.me>
2025-10-02 22:25:41 +0200

    feat: create gitkeep automatically

--- lua/typstnote.lua
+++ lua/typstnote.lua
@@ -4,11 +4,13 @@ local M = {}
 ---@field note_root string Note root directory. (default: research/)
 ---@field paper_directory string Where your notes on papers will be stored. (default: <root>/papers/)
 ---@field idea_directory string Where your notes on ideas will be stored. (default: <root>/ideas/)
+---@field create_gitkeep boolean If `.gitkeep` files should be generated in the directories. (default: true)
 
 M.default_config = {
 	note_root = "research",
 	paper_directory = "papers",
 	idea_directory = "ideas",
+	create_gitkeep = true,
 }
 
 --- Sets the paths for the creation
@@ -18,24 +20,9 @@ M.init_config = function(config)
 	config = config or M.default_config
 	config.paper_directory = config.note_root .. "/" .. config.paper_directory
 	config.idea_directory = config.note_root .. "/" .. config.idea_directory
+	config.create_gitkeep = true
 
 	return config
-end
-
---- Setup function for Lazyvim
----@param opts typstnote.Configuration? Plugin configuration
-M.setup = function(opts)
-	opts = opts or {
-		note_root = "research",
-		paper_directory = "papers",
-		idea_directory = "papers",
-	}
-
-	opts.paper_directory = opts.note_root .. "/" .. opts.paper_directory
-	opts.idea_directory = opts.note_root .. "/" .. opts.idea_directory
-
-	-- user configuration
-	print("[typstnote.nvim] called setup")
 end
 
 --- Creates all necessary directories in the current folder
@@ -49,11 +36,23 @@ M.create_directories = function(config)
 
 	for _, dir in ipairs(directories) do
 		local path = vim.fs.root(0, ".git") .. "/" .. dir
-		print(path)
+
 		os.execute("mkdir -p " .. path)
-		os.execute("touch " .. path .. "/.gitkeep")
+		print(path)
+
+		if config.create_gitkeep then
+			os.execute("touch " .. path .. "/.gitkeep")
+		end
 	end
+
 	print("directories initialized")
+end
+
+---@param opts typstnote.Configuration? Plugin configuration
+--- Setup function for Lazyvim
+M.setup = function(opts)
+	opts = M.init_config(opts)
+	-- user configuration
 end
 
 vim.api.nvim_create_user_command("CreateDirectories", function()
